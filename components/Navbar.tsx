6d7e4d864b7800e3bd09691e5dda54eadcccf539
Matheus <lima.eca@gmail.com>
2023-06-28 22:19:10 -0300

    responsive navbar with search field

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,30 +2,51 @@ import React from 'react';
 import styles from '@/styles/components/Navbar.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation'
 
 console.log(styles.headerContainer);
 
-const Header = (): React.ReactElement => (
-  <nav>
-    <div className={styles.mainContainer}>
-      <div className={styles.contentWrapper}>
-        <div className={styles.logoContainer}>
-          <Image src='/pokeball-icon.png' width={50} height={50} alt="pokeball icon" />
-          <div>
-            PokeNext
+const Header = (): React.ReactElement => {
+  const [searchValue, setSearch] = React.useState<string>('');
+  const router = useRouter();
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    console.log(event);
+    if (event.key !== 'Enter') return;
+    router.push(`search/${searchValue}`);
+    setSearch('');
+  }
+  return (
+    <nav>
+      <div className={styles.mainContainer}>
+        <div className={styles.contentWrapper}>
+          <div className={styles.logoContainer} onClick={() => router.push('/')}>
+            <Image src='/pokeball-icon.png' width={50} height={50} alt="pokeball icon" />
+            <div>
+              PokéNext
+            </div>
           </div>
+          <div className={styles.searchBar}>
+            <input
+              placeholder='Search Pokémon'
+              value={searchValue}
+              type='text'
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSearch(event.target.value)}
+              onKeyUp={handleKeyPress}
+            />
+          </div>
+          <ul className={styles.navigationContainer}>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/about'>About</Link>
+            </li>
+          </ul>
         </div>
-        <ul className={styles.navigationContainer}>
-          <li>
-            <Link href='/'>Home</Link>
-          </li>
-          <li>
-            <Link href='/about'>About</Link>
-          </li>
-        </ul>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Header;
