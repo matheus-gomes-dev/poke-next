f55eabb2f3ef62addfeb65641ba2722174c3a80b
Matheus Gomes <lima.eca@gmail.com>
2023-07-25 16:19:37 -0300

    progressbar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/Navbar';
 import { PokemonsProvider } from '@/context/pokemons';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PokemonsProvider>
+      <NextNProgress color='#FF1D1F' options={{ showSpinner: false }} />
       <Navbar />
       <Component {...pageProps} />
     </PokemonsProvider>
